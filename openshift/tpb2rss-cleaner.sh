610c62ab4f28d13c3866f93af524e23725ec5934
camporez <cadastros@camporez.com>
2014-07-08 20:59:09 -0300

    Add tpb2rss-cleaner.sh

---
+++ openshift/tpb2rss-cleaner.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+FILE="$OPENSHIFT_DATA_DIR/searches"
+DIR="$OPENSHIFT_DATA_DIR/raw"
+
+removefiles() {
+	test -f "$1" && rm -f "$1"
+	test -f "$( dirname "$1" )/../static/$( basename "$1" ).xml" && rm -f "$( dirname "$1" )/../static/$( basename "$1" ).xml"
+}
+
+for INPUT in $DIR/*; do
+	ITEM=$( basename "`sed -e 's/\[/\\\[/g' -e 's/\!/\\\!/g' <<< "$INPUT"`" )
+	grep -e ^"$ITEM$" "$FILE" || removefiles "$INPUT"
+done
