f25a37c99764a88af48c787f5095cad9fb8246cf
camporez <cadastros@camporez.com>
2014-07-10 11:28:05 -0300

    Included line breaks.

--- openshift/page/page-compiler.sh
+++ openshift/page/page-compiler.sh
@@ -3,7 +3,7 @@
 pythonize() {
 	sed -i 's/"/\\"/g' "$1"
 	sed -i 's/\t/\\t/g' "$1"
-	sed -i ':a;N;$!ba;s/\n/"\nhtml += "/g' "$1"
+	sed -i ':a;N;$!ba;s/\n/"\nhtml += "\\n/g' "$1"
 	sed -i '1s/^/html = \"/' "$1"
 	sed -i '$s/$/\"/' "$1"
 	sed -i ':a;N;$!ba;s/\"\nhtml += \"\"\n/\\n"\n/g' "$1"
