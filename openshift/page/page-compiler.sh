36bfe7db2b04955e8fd6165094076684eb053972
camporez <cadastros@camporez.com>
2014-07-30 22:59:31 -0300

    Importing the main page, instead of embed it on the wsgi file.

--- openshift/page/page-compiler.sh
+++ openshift/page/page-compiler.sh
@@ -3,10 +3,16 @@
 pythonize() {
 	sed -i 's/"/\\"/g' "$1"
 	sed -i 's/\t/\\t/g' "$1"
-	sed -i ':a;N;$!ba;s/\n/"\nhtml += "\\n/g' "$1"
-	sed -i '1s/^/html = \"/' "$1"
+	sed -i ':a;N;$!ba;s/\n/"\n\thtml += "\\n/g' "$1"
+	sed -i '1s/^/\thtml = \"/' "$1"
 	sed -i '$s/$/\"/' "$1"
-	sed -i ':a;N;$!ba;s/\"\nhtml += \"\"\n/\\n"\n/g' "$1"
+	sed -i ':a;N;$!ba;s/\"\n\thtml += \"\"\n/\\n"\n/g' "$1"
+	sed -i '1i def build(xml):' "$1"
+	sed -i '$a\\treturn html' "$1"
+	for line in "$( grep -in "Page not found" "$1" | cut -f 1 -d ':' )"; do
+		nf=$( cat "$1" | head -n $line | tail -n 1 | sed 's/\//\\\//g' | sed 's/\\n/\\\\n/g' | sed 's/\\t/\\\\t/g' | sed 's/\\"/\\\\"/g' )
+		sed -i "s/.*not found.*/\tif xml == None:\n\t$nf/g" "$1"
+	done
 }
 
-cp tpb2rss.html tpb2rss.py && pythonize tpb2rss.py
+cp page.html ../page.py && pythonize ../page.py
